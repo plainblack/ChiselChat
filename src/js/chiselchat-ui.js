af7b66f227061843968f6bb8ac80375697285883
JT Smith <jt@plainblack.com>
2014-10-28 15:24:57 -0500

    added sounds to chat

--- src/js/chiselchat-ui.js
+++ src/js/chiselchat-ui.js
@@ -1,5 +1,38 @@
 (function($, window) {
 
+  function getCookie( name ) {	
+	var start = document.cookie.indexOf( name + "=" );
+	var len = start + name.length + 1;
+	if ( ( !start ) && ( name != document.cookie.substring( 0, name.length ) ) ) {
+		return null;
+	}
+	if ( start == -1 ) return null;
+	var end = document.cookie.indexOf( ';', len );
+	if ( end == -1 ) end = document.cookie.length;
+	return unescape( document.cookie.substring( len, end ) );
+  }
+
+  function setCookie( name, value, expires, path, domain, secure ) {
+	var today = new Date();
+	today.setTime( today.getTime() );
+	if ( expires ) {
+		expires = expires * 1000 * 60 * 60 * 24;
+	}
+	var expires_date = new Date( today.getTime() + (expires) );
+	document.cookie = name+'='+escape( value ) +
+		( ( expires ) ? ';expires='+expires_date.toGMTString() : '' ) + //expires.toGMTString()
+		( ( path ) ? ';path=' + path : '' ) + 
+		( ( domain ) ? ';domain=' + domain : '' ) +
+		( ( secure ) ? ';secure' : '' );
+  }
+
+  function deleteCookie( name, path, domain ) {
+	if ( getCookie( name ) ) document.cookie = name + '=' +
+			( ( path ) ? ';path=' + path : '') +
+			( ( domain ) ? ';domain=' + domain : '' ) +
+			';expires=Thu, 01-Jan-1970 00:00:01 GMT';
+  }
+
   var title_original = document.title;
   var title_timeout;
 
@@ -65,6 +98,9 @@
       
     // A toggle so panes can be full screen
     this.fullScreenTab = false;
+      
+    // A toggle so sounds can be played
+    this.playSounds = getCookie("chiselchat-playSounds") === 'true' ? true : false;
       
     // A toggle to not display new message counts right after login
     this.displayNewMessageCount = false;
@@ -847,6 +883,24 @@
       return false;
     });
       
+    // Handle toggle of sound
+    $(document).delegate('[data-event="chiselchat-toggle-sound"]', 'click', function(event) {
+      var control = $('.chiselchat-sound-control');
+      if (self.playSounds) {
+          self.playSounds = false;
+          setCookie("chiselchat-playSounds", false, 30);
+          control.removeClass('chiselchat-glyphicon-volume-up');
+          control.addClass('chiselchat-glyphicon-volume-off');
+      }
+      else {
+          self.playSounds = true;
+          setCookie("chiselchat-playSounds", true, 30);
+          control.addClass('chiselchat-glyphicon-volume-up');
+          control.removeClass('chiselchat-glyphicon-volume-off');
+      }
+      return false;
+    });
+      
       
   };
 
@@ -1093,6 +1147,14 @@ ChiselchatUI.prototype.executeCommands = function(message) {
         // set message count to 0
         self.resetNewMessageCount(roomId); 
       }
+
+    // render the sound control
+    if (this.playSounds) {
+        $('.chiselchat-sound-control').addClass('chiselchat-glyphicon-volume-up');
+    }
+    else {
+        $('.chiselchat-sound-control').addClass('chiselchat-glyphicon-volume-off');
+    }
   };
 
   /**
@@ -1198,6 +1260,7 @@ ChiselchatUI.prototype.executeCommands = function(message) {
           $newCount.html(parseInt($newCount.html(),10) + 1);
           this.pulse($newCount, 'chiselchat-new-count-alert', { pulses : 5, duration : 300 });
           window.flashTitle('New Chat Message!');
+          this.playNewMessageSound();
       }        
         
         
@@ -1269,7 +1332,12 @@ ChiselchatUI.prototype.executeCommands = function(message) {
       .replace(self.urlPattern, '<a target="_blank" href="$&">$&</a>')
       .replace(self.pseudoUrlPattern, '$1<a target="_blank" href="http://$2">$2</a>');
   };
-    
+
+  ChiselchatUI.prototype.playNewMessageSound = function() {
+      if (this.playSounds) {
+	document.getElementById('chiselchat-new-message-sound').play();
+      }
+  };    
 
   ChiselchatUI.prototype.pulse = function(selector, classname, options, callback) {
   var defaults = {
